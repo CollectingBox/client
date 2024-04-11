3d226634542ad709fb37c2c01e6b89b8a56bbcb9
cham0287 <crtmt97@gmail.com>
2024-04-11 23:02:04 +0900

    fix: fix build error

--- src/service/profile.ts
+++ src/service/profile.ts
@@ -1,4 +1,4 @@
-import { ICollection } from 'types/collection';
+import { ICollection } from '@/types/collection';
 
 export const getProfiles = async (): Promise<ICollection[]> => {
 	return fetch('http://localhost:9090/api/profiles').then((res) => res.json());
