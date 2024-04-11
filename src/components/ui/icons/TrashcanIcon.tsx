16caf46745043cf6dfd861ef0c45700629b395be
cham0287 <crtmt97@gmail.com>
2024-04-11 22:19:51 +0900

    Chore: fix typo

--- src/components/ui/icons/TrashcanIcon.tsx
+++ src/components/ui/icons/TrashcanIcon.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import TrashcanSvg from '@/public/icons/Trashcan.svg';
+import TrashcanSvg from '@/public/icons/trashcan.svg';
 import Image from 'next/image';
 
 const TrashcanIcon = ({ size }: { size: number }) => (
-	<Image src={TrashcanSvg} alt="Trashcan icon" width={size} height={size} />
+	<Image src={TrashcanSvg} alt="trashcan icon" width={size} height={size} />
 );
 
 export default TrashcanIcon;
