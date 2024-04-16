8662b7fcdf2f9185f238f7521d4ca1f7814935aa
nangniya <apinksky00@gmail.com>
2024-04-16 21:07:49 +0900

    feat: 필터버튼 반응형

--- src/components/FilterButtons.tsx
+++ src/components/FilterButtons.tsx
@@ -18,14 +18,23 @@ const FilterButtons = () => {
 		${selectedFilters.includes(filter) ? color + ' text-white' : 'bg-white text-Gray-200'}`;
 	};
 
+	const filterTextStyle = (filter: string) => {
+		return `xl:block ${selectedFilters.includes(filter) ? 'block' : 'hidden'}`;
+	};
+
 	const handleClickFilter = (e: MouseEvent<HTMLButtonElement>) => {
-		const { value } = e.target as HTMLButtonElement;
+		const { value } = e.currentTarget as HTMLButtonElement;
 		setSelectedFilters((prev) => {
 			if (!prev.includes(value)) {
 				return [...prev, value];
 			}
 			return prev.filter((filter) => filter !== value);
 		});
+	};
+
+	const handleChildClick = (e: MouseEvent<HTMLElement>) => {
+		const child = e.target.dispatchEvent;
+		console.log(child);
 	};
 
 	return (
@@ -35,36 +44,42 @@ const FilterButtons = () => {
 				onClick={handleClickFilter}
 				className={filterButtonStyle('폐의류', 'bg-Green-400')}
 			>
-				<ClothesIcon enabled={selectedFilters.includes('폐의류')} /> 폐의류
+				<ClothesIcon enabled={selectedFilters.includes('폐의류')} />
+				<p className={filterTextStyle('폐의류')}>폐의류</p>
 			</button>
 			<button
 				value="폐형광등"
 				onClick={handleClickFilter}
 				className={filterButtonStyle('폐형광등', 'bg-Blue-100')}
 			>
-				<FluorescentLampIcon enabled={selectedFilters.includes('폐형광등')} />
-				폐형광등
+				<span className="flex" onClick={handleChildClick}>
+					<FluorescentLampIcon enabled={selectedFilters.includes('폐형광등')} />
+					<p className={filterTextStyle('폐형광등')}>폐형광등</p>
+				</span>
 			</button>
 			<button
 				value="폐건전지"
 				onClick={handleClickFilter}
 				className={filterButtonStyle('폐건전지', 'bg-Red-100')}
 			>
-				<BatteryIcon enabled={selectedFilters.includes('폐건전지')} /> 폐건전지
+				<BatteryIcon enabled={selectedFilters.includes('폐건전지')} />
+				<p className={filterTextStyle('폐건전지')}>폐건전지</p>
 			</button>
 			<button
 				value="폐의약품"
 				onClick={handleClickFilter}
 				className={filterButtonStyle('폐의약품', 'bg-Brown-100 ')}
 			>
-				<PillIcon enabled={selectedFilters.includes('폐의약품')} /> 폐의약품
+				<PillIcon enabled={selectedFilters.includes('폐의약품')} />
+				<p className={filterTextStyle('폐의약품')}>폐의약품</p>
 			</button>
 			<button
 				value="쓰레기통"
 				onClick={handleClickFilter}
 				className={filterButtonStyle('쓰레기통', 'bg-Gray-400')}
 			>
-				<TrashcanIcon enabled={selectedFilters.includes('쓰레기통')} /> 쓰레기통
+				<TrashcanIcon enabled={selectedFilters.includes('쓰레기통')} />
+				<p className={filterTextStyle('쓰레기통')}>쓰레기통</p>
 			</button>
 		</div>
 	);
