56beac1a60b156977e7d6952ba9e1b0d8bc1376b
nangniya <apinksky00@gmail.com>
2024-04-16 17:14:57 +0900

    feat: drag handle UI 추가

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -19,9 +19,11 @@ const Sidebar = () => {
 			className={`fixed bottom-0 w-full xl:static rounded-t-[32px] xl:rounded-none flex flex-col ${isOpen ? 'xl:w-[390px]' : 'xl:w-[86px]'} z-10 bg-white transition-all duration-1000`}
 			onClick={() => setIsOpen(!isOpen)}
 		>
-			<div className="h-S-24 rounded-t-[32px] bg-white xl:hidden"></div>
+			<div className="flex justify-center items-end h-S-24 rounded-t-[32px] bg-white xl:hidden">
+				<div className="w-S-48 h-S-4 bg-Gray-200 rounded-full" />
+			</div>
 			<div
-				className={`hidden xl:flex bg-white  ${isOpen ? 'px-S-28' : 'px-S-20'} pt-S-24 pb-S-16 transition-all duration-1000`}
+				className={`hidden xl:flex bg-white ${isOpen ? 'px-S-28' : 'px-S-20'} pt-S-24 pb-S-16 transition-all duration-1000`}
 			>
 				<Link href="/" className="flex items-center gap-2 w-min">
 					<LogoIcon />
