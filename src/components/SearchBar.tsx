6f9f2b92fa33c74446d9c4ae64cb3c5f64137c10
nangniya <apinksky00@gmail.com>
2024-04-18 21:44:54 +0900

    feat: 검색결과 없을때 토스트

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import { OpenContext } from '../app/open-provider';
+import ToastError from './ui/toasts/ToastError';
 
 interface Props {
 	setCenter: Dispatch<SetStateAction<{ lat: number; lng: number }>>;
@@ -11,6 +13,7 @@ const SearchBar = ({ setCenter }: Props) => {
 		null,
 	);
 
+	const [isError, setIsError] = useState(false);
 	useEffect(() => {
 		kakao.maps.load(() => {
 			setGeocoder(new kakao.maps.services.Geocoder());
@@ -31,11 +34,15 @@ const SearchBar = ({ setCenter }: Props) => {
 					const xstr = data[0].x;
 					const ystr = data[0].y;
 					setCenter({ lat: Number(ystr), lng: Number(xstr) });
-				} else {
-					console.log('검색된 장소에 동이나 구가 포함되어 있지 않습니다.');
+					return;
 				}
+				const xstr = data[0].x;
+				const ystr = data[0].y;
+				setCenter({ lat: Number(ystr), lng: Number(xstr) });
 			} else {
 				console.log('검색된 장소가 없습니다.');
+				setIsError(true);
+				setTimeout(() => setIsError(false), 3000);
 			}
 		});
 	};
@@ -47,14 +54,22 @@ const SearchBar = ({ setCenter }: Props) => {
 	};
 
 	return (
-		<input
-			className="px-[16px] py-[14px] w-[328px] max-w-[360px] rounded-[16px] border-[1.5px] placeholder:Body-Large placeholder:text-Gray-200 Title-Small text-Gray-800 border-Green-400 Elevation-2-Bottom"
-			type="search"
-			placeholder="동네명 검색 (Ex. 종로구, 상수동)"
-			value={value}
-			onChange={(e) => setValue(e.target.value)}
-			onKeyDown={handleKeyDown}
-		/>
+		<>
+			<input
+				className="px-[16px] py-[14px] w-[328px] max-w-[360px] rounded-[16px] border-[1.5px] placeholder:Body-Large placeholder:text-Gray-200 Title-Small text-Gray-800 border-Green-400 Elevation-2-Bottom"
+				type="search"
+				placeholder="동네명 검색 (Ex. 종로구, 상수동)"
+				value={value}
+				onChange={(e) => setValue(e.target.value)}
+				onKeyDown={handleKeyDown}
+			/>
+			{isError && (
+				<ToastError
+					title="검색 결과가 없습니다"
+					description="검색어를 다시 확인해주세요"
+				/>
+			)}
+		</>
 	);
 };
 
---
+++ src/components/ui/toasts/ToastError.tsx
@@ -0,0 +1,21 @@
+import Alert from '../icons/Alert';
+
+interface Props {
+	title: string;
+	description: string;
+	className?: string;
+}
+
+export default function ToastError({ title, description, className }: Props) {
+	return (
+		<div
+			className={`${className ? className : ''} fixed bottom-[50px] xl:bottom-[S-24] left-S-12 xl:left-[110px] w-[95dvw] xl:w-[360px] flex gap-S-8 px-S-20 py-S-12 z-50 rounded-lg bg-[#92908e] opacity-70 Elevation-2-Bottom`}
+		>
+			<Alert color="white" />
+			<div className="flex flex-col gap-S-4">
+				<p className="Title-Small text-white">{title}</p>
+				<p className="Label-Large text-white">{description}</p>
+			</div>
+		</div>
+	);
+}
