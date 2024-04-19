042e16873ee5c72a15a68269ccae2f35d8669960
cham0287 <crtmt97@gmail.com>
2024-04-20 00:03:27 +0900

    feat: mock data 추가, filter별 marker icon 추가

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,6 +1,9 @@
 import { CollectionTags } from '@/types/define';
 import PillMarkerPng from '@/public/icons/pillMarker.png';
 import FluorescentLampMarkerPng from '@/public/icons/fluorescentLampMarker.png';
+import ClothesMarker from '@/public/icons/clothesMarker.png';
+import BatteryMarker from '@/public/icons/batteryMarker.png';
+import TrashcanMarker from '@/public/icons/trashcanMarker.png';
 import { useKakaoLoader as useKakaoLoaderOrigin } from 'react-kakao-maps-sdk';
 
 export default function useKakaoLoader() {
@@ -11,13 +14,17 @@ export default function useKakaoLoader() {
 
 export const getMarkerUrl = (tag: CollectionTags): string => {
 	switch (tag) {
+		case '폐의류':
+			return ClothesMarker.src;
 		case '폐의약품':
 			return PillMarkerPng.src;
 		case '폐형광등':
 			return FluorescentLampMarkerPng.src;
+		case '폐건전지':
+			return BatteryMarker.src;
+		case '쓰레기통':
+			return TrashcanMarker.src;
 		default:
-			return FluorescentLampMarkerPng.src;
-
-		// throw new Error('잘못된 태그입니다'); // NOTE: 나머지 3개 marker icon 추가되면 복구
+			throw new Error('잘못된 태그입니다');
 	}
 };
