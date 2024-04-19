0879c3d96246d27a37f7b79db80fdd7450e34862
cham0287 <crtmt97@gmail.com>
2024-04-19 18:26:51 +0900

    Design: ToastError max width 설정

--- src/components/ui/toasts/ToastError.tsx
+++ src/components/ui/toasts/ToastError.tsx
@@ -9,7 +9,7 @@ interface Props {
 export default function ToastError({ title, description, className }: Props) {
 	return (
 		<div
-			className={`${className ? className : ''} fixed bottom-[50px] xl:bottom-[S-24] left-S-12 xl:left-[110px] w-[95dvw] xl:w-[360px] flex gap-S-8 px-S-20 py-S-12 z-50 rounded-lg bg-[#92908e] opacity-70 Elevation-2-Bottom`}
+			className={`${className ? className : ''} fixed bottom-[50px] max-w-[400px] xl:bottom-[S-24] left-S-12 xl:left-[110px] w-[95dvw] xl:w-[360px] flex gap-S-8 px-S-20 py-S-12 z-50 rounded-lg bg-[#92908e] opacity-70 Elevation-2-Bottom`}
 		>
 			<Alert color="white" />
 			<div className="flex flex-col gap-S-4">
