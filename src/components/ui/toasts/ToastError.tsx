2361167a35023e88f6b28adb9946a642721c4c38
nangniya <apinksky00@gmail.com>
2024-04-22 10:55:04 +0900

    feat: 한개이상필터토스트 사라짐 애니메이션

--- src/components/ui/toasts/ToastError.tsx
+++ src/components/ui/toasts/ToastError.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import Alert from '../icons/Alert';
 
 interface Props {
@@ -7,9 +10,19 @@ interface Props {
 }
 
 export default function ToastError({ title, description, className }: Props) {
+	const [isVisible, setIsVisible] = useState(true);
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setIsVisible(false);
+		}, 2500);
+
+		return () => clearTimeout(timer);
+	}, []);
 	return (
 		<div
-			className={`${className ? className : ''} fixed bottom-[50px] max-w-[400px] xl:bottom-[S-24] left-S-12 xl:left-[110px] w-[95dvw] xl:w-[360px] flex gap-S-8 px-S-20 py-S-12 z-50 rounded-lg bg-[#92908e] opacity-70 Elevation-2-Bottom`}
+			className={`${className ? className : ''} ${
+				isVisible ? 'opacity-70' : 'opacity-0'
+			} fixed bottom-[50px] max-w-[400px] xl:bottom-[S-24] left-S-12 xl:left-[110px] w-[95dvw] xl:w-[360px] flex gap-S-8 px-S-20 py-S-12 z-50 rounded-lg bg-[#92908e] Elevation-2-Bottom transition-opacity duration-500`}
 		>
 			<Alert color="white" />
 			<div className="flex flex-col gap-S-4">
