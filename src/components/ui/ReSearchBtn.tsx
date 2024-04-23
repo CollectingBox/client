83604082b7090ff1cfba5106b2f137b39b9efe85
nangniya <apinksky00@gmail.com>
2024-04-23 15:19:57 +0900

    fix: 이 지역 재검색 z-index 바텀시트 안가리게 조정

--- src/components/ui/ReSearchBtn.tsx
+++ src/components/ui/ReSearchBtn.tsx
@@ -12,7 +12,7 @@ const ReSearchBtn = ({ setIsMoved }: Props) => {
 			onClick={() => {
 				setIsMoved(false);
 			}}
-			className="fixed bottom-[50px] mx-auto left-0 right-0 w-max xl:bottom-[S-24] z-50 flex gap-S-4 justify-between py-S-12 px-S-20 bg-Green-400 text-white Title-Small rounded-[32px] Elevation-2-Bottom"
+			className="fixed bottom-[50px] mx-auto left-0 right-0 w-max xl:bottom-[S-24] z-10 flex gap-S-4 justify-between py-S-12 px-S-20 bg-Green-400 text-white Title-Small rounded-[32px] Elevation-2-Bottom"
 		>
 			<Refresh color="white" />이 지역 재검색
 		</button>
