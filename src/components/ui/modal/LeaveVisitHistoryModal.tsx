e68684996d6df3a8f3a46d4ceca4d0770a824687
cham0287 <crtmt97@gmail.com>
2024-04-29 05:31:30 +0900

    design: 방문 기록 모달 max width

--- src/components/ui/modal/LeaveVisitHistoryModal.tsx
+++ src/components/ui/modal/LeaveVisitHistoryModal.tsx
@@ -17,7 +17,7 @@ const LeaveVisitHistoryModal = ({ setIsModalOpen }: Props) => {
 			onClick={() => setIsModalOpen(false)}
 			className="xl:p-S-26 fixed inset-0 z-50 bg-black bg-opacity-50 p-S-16 xl:right-[calc(100dvw-390px)]"
 		>
-			<div className="w-100 relative left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col gap-6 rounded-2xl bg-white px-S-24 py-S-28">
+			<div className="w-100 relative left-1/2 top-1/2 flex max-w-[400px] -translate-x-1/2 -translate-y-1/2 flex-col gap-6 rounded-2xl bg-white px-S-24 py-S-28">
 				<header className="flex justify-between">
 					<h3 className="mx-0 flex-1 Title-Medium">방문 기록 남기기</h3>
 					<button onClick={() => setIsModalOpen(false)}>
