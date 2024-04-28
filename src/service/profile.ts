05e07f8eaf881801cef1abf60905b503998b5d1b
cham0287 <crtmt97@gmail.com>
2024-04-29 05:37:34 +0900

    remove: 미사용 파일 제거

--- src/service/profile.ts
+++
@@ -1,5 +0,0 @@
-import { ICollection } from '@/types/collection';
-
-export const getProfiles = async (): Promise<ICollection[]> => {
-	return fetch('http://localhost:9090/api/profiles').then((res) => res.json());
-};
