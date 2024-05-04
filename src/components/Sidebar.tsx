0e5aca9bfd5e666fef4fb6e10d45dced3671506c
cham0287 <crtmt97@gmail.com>
2024-05-04 16:43:12 +0900

    design: sidebar, botttomSheet 분리에 따른 className 정리

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -36,26 +36,14 @@ const Sidebar = () => {
 
 	return (
 		<aside
-			className={`${collectionDetailDTO?.data ? 'visible' : 'invisible'} 
-			fixed bottom-0 left-0 right-0 flex h-[80dvh]
-			flex-col rounded-t-[32px]
-			xl:visible xl:relative xl:h-[100dvh] xl:rounded-none 
-			${isSidebarOpen ? 'xl:w-[390px]' : 'xl:w-[86px] xl:translate-y-0'} 
-			${isSidebarOpen ? 'translate-y-0' : 'translate-y-[75dvh]'}
-			Elevation-2-Top z-20 bg-white transition-all duration-500 xl:Elevation-4-Bottom`}
+			className={`relative z-20 flex h-[100dvh] flex-col bg-white  
+			${isSidebarOpen ? 'w-[390px]' : 'w-[86px]'} transition-all duration-500`}
 		>
-			<button
-				onClick={handleToggleSidebar}
-				className="flex h-S-24 items-end justify-center rounded-t-[32px] bg-white py-6 xl:hidden"
-			>
-				<div className="h-S-4 w-S-48 rounded-full bg-Gray-200" />
-			</button>
-
-			<div className="max-h-[calc(100dvh_-_150px)] overflow-y-scroll scrollbar-hide xl:max-h-[calc(100dvh_-_24px)]">
+			<div className="max-h-[calc(100dvh_-_24px)] overflow-y-scroll scrollbar-hide">
 				<Link
 					href="/"
-					className={`hidden items-center gap-2 xl:flex ${isSidebarOpen ? 'px-S-28' : 'px-S-20'} 
-					pb-S-16 pt-S-24 transition-all duration-1000`}
+					className={`flex items-center gap-2 pb-S-16 pt-S-24
+					${isSidebarOpen ? 'px-S-28' : 'px-S-20'} transition-all duration-1000`}
 				>
 					<LogoIcon />
 					{isSidebarOpen && <LogoWordIcon />}
@@ -64,8 +52,7 @@ const Sidebar = () => {
 				{isSidebarOpen && collectionDetailDTO?.data && (
 					<>
 						<article
-							className={`flex flex-col gap-3 bg-Gray-50 xl:w-[390px] xl:pt-S-12 
-							${isSidebarOpen ? 'xl:translate-x-0' : 'xl:-translate-x-[304px]'} transition-all duration-1000`}
+							className={`flex w-[390px] flex-col gap-3 bg-Gray-50 pt-S-12`}
 						>
 							<BoxInformation collectionDetail={collectionDetailDTO?.data} />
 							<VisitRecord reviews={collectionDetailDTO?.data.reviews} />
@@ -80,8 +67,9 @@ const Sidebar = () => {
 			{/** Sidebar 여닫기 버튼 */}
 			{collectionDetailDTO?.data && (
 				<div
-					className={`hidden h-S-56 w-S-24 rounded-br rounded-tr bg-white transition-all duration-1000 
-					xl:absolute xl:right-[-24px] xl:top-1/2 xl:flex xl:-translate-y-1/2 xl:items-center`}
+					className={`absolute right-[-24px] top-1/2 flex
+					h-S-56 w-S-24 -translate-y-1/2 items-center 
+					rounded-br rounded-tr bg-white`}
 					onClick={handleToggleSidebar}
 				>
 					{isSidebarOpen ? <Back /> : <Front />}
--- src/components/ui/ReSearchBtn.tsx
+++ src/components/ui/ReSearchBtn.tsx
@@ -13,7 +13,7 @@ const ReSearchBtn = ({ onClick }: Props) => {
 	return (
 		<button
 			onClick={onClick}
-			className={`Elevation-2-Bottom fixed bottom-[50px] left-0 right-0 z-10 mx-auto flex w-max justify-between gap-S-4 rounded-[32px] bg-Green-400 px-S-20 py-S-12 text-white Title-Small 
+			className={`z-5 Elevation-2-Bottom fixed bottom-[50px] left-0 right-0 mx-auto flex w-max justify-between gap-S-4 rounded-[32px] bg-Green-400 px-S-20 py-S-12 text-white Title-Small 
 			${!isSidebarOpen && 'xl:left-[calc(50dvw_+_calc(43px)_-_calc(80px))] xl:right-[calc(50dvw_-_calc(195px)_+_calc(80px))]'}
 			${isSidebarOpen && 'xl:left-[calc(50dvw_+_calc(195px)_-_calc(80px))] xl:right-[calc(50dvw_-_calc(195px)_+_calc(80px))]'} 
 			transition-all duration-500`}
