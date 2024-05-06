6e98eb840078c8ad10a54f938bed9609949d3bd2
cham0287 <crtmt97@gmail.com>
2024-05-06 09:51:58 +0900

    feat: 처리방법 accordion shadcn ui로 수정

--- src/components/ui/sidebars/DiscardMethod.tsx
+++ src/components/ui/sidebars/DiscardMethod.tsx
@@ -1,7 +1,10 @@
-import { useState } from 'react';
-import Down from '../icons/Down';
-import Up from '../icons/Up';
 import { methodTypes } from '@/utils/methodTypes';
+import {
+	Accordion,
+	AccordionContent,
+	AccordionItem,
+	AccordionTrigger,
+} from '../shadcn-ui/accordion';
 
 interface Props {
 	tag: string;
@@ -9,43 +12,32 @@ interface Props {
 
 export default function DiscardMethod({ tag }: Props) {
 	const DM = methodTypes.filter((item) => item.tag === tag)[0].methods;
-	const [openIndex, SetOpenIndex] = useState(-1);
 	return (
 		<section className="flex flex-col gap-S-20 bg-white p-S-24">
 			<h5 className="text-Gray-700 Title-Small">배출 방법</h5>
-			<div className="flex flex-col gap-S-14">
+			<Accordion className="flex flex-col gap-4" type="single" collapsible>
 				{DM.map((method, index) => (
-					<div
-						key={index}
-						className="flex flex-col gap-S-16 rounded-[8px] bg-Green-50 p-S-16"
+					<AccordionItem
+						key={method.title}
+						className="rounded-[8px] bg-Green-50  px-S-12"
+						value={`item-${index}`}
 					>
-						<div className="flex justify-between">
-							<p className="text-Green-500 Title-Small">{method.title}</p>
-							{openIndex !== index ? (
-								<span onClick={() => SetOpenIndex(index)}>
-									<Down color="#45995E" />
-								</span>
-							) : (
-								<span onClick={() => SetOpenIndex(-1)}>
-									<Up color="#45995E" />
-								</span>
-							)}
-						</div>
-						{openIndex === index && (
-							<div className="bg-transparent">
-								{method.descriptions.map((content, index) => (
-									<p
-										key={`description-${index}`}
-										className="text-Gray-600 Label-Large-Custom"
-									>
-										{content}
-									</p>
-								))}
-							</div>
-						)}
-					</div>
+						<AccordionTrigger className="text-Green-500 Title-Small">
+							{method.title}
+						</AccordionTrigger>
+						<AccordionContent className="text-Gray-600">
+							{method.descriptions.map((content, index) => (
+								<p
+									key={`description-${index}`}
+									className="text-Gray-600 Label-Large-Custom"
+								>
+									{content}
+								</p>
+							))}
+						</AccordionContent>
+					</AccordionItem>
 				))}
-			</div>
+			</Accordion>
 		</section>
 	);
 }
